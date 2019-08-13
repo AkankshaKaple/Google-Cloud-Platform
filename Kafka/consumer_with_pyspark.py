415b66416d02bf66e648497b7ab23a3d590f16a9
AkankshaKaple <akankshakaple@gmail.com>
2019-08-13 19:55:35 +0530

    Kafka consumer with pyspark

--- Kafka/consumer_with_pyspark.py
+++ Kafka/consumer_with_pyspark.py
@@ -13,18 +13,11 @@ sc.setLogLevel("WARN")
 ssc = StreamingContext(sc, 5)
 
 kafkaStream = KafkaUtils.createStream(ssc, 'cdh57-01-node-01.moffatt.me:2181'
-                                      , 'spark-streaming-group-id', {'twitter':1})
+                                      , 'spark-streaming-group-id', {'stockprice':1})
 
 parsed = kafkaStream.map(lambda v: json.loads(v[1]))
 
 parsed.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
 
-authors_dstream = parsed.map(lambda tweet: tweet['user']['screen_name'])
-
-author_counts = authors_dstream.countByValue()
-author_counts.pprint()
-
-author_counts_sorted_dstream = author_counts.transform(\
-  (lambda foo:foo\
-   .sortBy(lambda x:( -x[1]))))
-author_counts_sorted_dstream.pprint()
+ssc.start()
+ssc.awaitTermination()
