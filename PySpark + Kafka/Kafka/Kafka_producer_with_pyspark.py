b93be023b98bf96286ac23230dba797696615fd1
AkankshaKaple <akankshakaple@gmail.com>
2019-08-23 19:38:58 +0530

    Kafka producer using pyspark and spark streaming

---
+++ PySpark + Kafka/Kafka/Kafka_producer_with_pyspark.py
@@ -0,0 +1,30 @@
+#    Spark
+from pyspark import SparkContext
+#    Spark Streaming
+from pyspark.streaming import StreamingContext
+#    Kafka
+from pyspark.streaming.kafka import KafkaUtils
+#    json parsing
+import json
+
+sc = SparkContext(appName="PythonSparkStreamingKafka_RM_01")
+sc.setLogLevel("WARN")
+
+ssc = StreamingContext(sc, 60)
+
+kafkaStream = KafkaUtils.createStream(ssc, 'cdh57-01-node-01.moffatt.me:2181'
+                                      , 'spark-streaming', {'twitter':1})
+
+parsed = kafkaStream.map(lambda v: json.loads(v[1]))
+
+parsed.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
+
+authors_dstream = parsed.map(lambda tweet: tweet['user']['screen_name'])
+
+author_counts = authors_dstream.countByValue()
+author_counts.pprint()
+
+author_counts_sorted_dstream = author_counts.transform(\
+  (lambda foo:foo\
+   .sortBy(lambda x:( -x[1]))))
+author_counts_sorted_dstream.pprint()
